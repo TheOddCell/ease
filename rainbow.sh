3be5d2257c9c740d564c9c97a44b7626a6994f72
TheOddCell <me@oddcell.ca>
2025-11-28 17:56:51 -0500

    h

---
+++ rainbow.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ease -p "\zc222,222,222\z\zb255,255,255\zoddcell.ca      "; ease -p "\zb255,255,255\z  \zb255,0,0\z  \zb255,100,0\z  \zb255,255,0\z  \zb0,255,00\z  \zb0,0,255\z  \zb255,0,255\z  \zb255,255,255\z  ";ease -p "\zb255,255,255\z         rainbow";
